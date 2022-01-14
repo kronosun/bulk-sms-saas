ef3f5700efc00411ca0d4c83f275b4ec34394943
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-01-14 11:47:01 +0100

    integrated bootstrap to mail

--- resources/views/mail/user-regmail.blade.php
+++ resources/views/mail/user-regmail.blade.php
@@ -1,3 +1,5 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <style>
 	.auth-btn{
 		font-weight: 600; background-color: #1d2840; color: white; padding: 7px 12px; border: 1px solid transparent; border-radius: 5px; text-decoration:none;
@@ -30,7 +32,7 @@
 	<p><a href="{{ url('verify-email/'.$email.'/'.$verification_code) }}" class="btn auth-btn">Verify Email</a></p>
 
 	<p>OR</p>
-	<p>Copy the link below and past it in your favourite browser and hit the enter button. That should do it. Winks. </p>
+	<p>Copy the link below and paste it in your favourite browser and hit the enter button. That should do it. Winks. </p>
 
 	<a href="{{ url('verify-email/'.$email.'/'.$verification_code) }}">{{ url('verify-email/'.$email.'/'.$verification_code) }}</a>
 <hr>
